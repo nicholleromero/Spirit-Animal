1768a3366c02747a0bd1cea52947c649b6053398
David Swaintek <dswaintek@gmail.com>
2015-10-19 12:27:13 -0700

    Second push of question.js

--- questions.js
+++ questions.js
@@ -1,6 +1,7 @@
-var Animal = function (animalName, strong, wise, resourceful, cunning, noble, loyal, loner, smallgroup, 
+var Animal = function (animalName, animalImage, strong, wise, resourceful, cunning, noble, loyal, loner, smallgroup, 
 	largegroup, water, land, arctic, forest, river, mountain) {
 	this.animalName = animalName;
+	this.animalImage = animalImage;
 	this.strong = strong;
 	this.wise = wise;
 	this.resourceful = resourceful;
@@ -19,12 +20,12 @@ var Animal = function (animalName, strong, wise, resourceful, cunning, noble, lo
 	}
 
 var animalData = [
-	['wolf', true, false, true, true, false, false, false, true, false, false, true, false, true, false, false],
-	['eagle', true, true, true, false, false, false, true, false, false, false, true, false, false, false, false],
-	['penguin', false, false, true, false, true, true, false, false, true, true, false, true, false, false, false],
-	['bear', true, true, false, true, false, false, true, false, false, false, true, false, true, false, false],
-	['squirrel', false, true, true, true, false, false, false, true, false, false, true, false, true, false, false],
-	['salmon', true, false, true, false, true, false, false, false, true, true, false, false, false, true, false]
+	['wolf', 'img/wolf.jpg', true, false, true, true, false, false, false, true, false, false, true, false, true, false, false],
+	['eagle', 'img/eagle.jpg', true, true, true, false, false, false, true, false, false, false, true, false, false, false, false],
+	['penguin', 'img/penguin,jpg', false, false, true, false, true, true, false, false, true, true, false, true, false, false, false],
+	['bear', 'img/bear.jpg', true, true, false, true, false, false, true, false, false, false, true, false, true, false, false],
+	['squirrel', 'img/squirrel.jpg', false, true, true, true, false, false, false, true, false, false, true, false, true, false, false],
+	['salmon', 'img/salmon.jpg', true, false, true, false, true, false, false, false, true, true, false, false, false, true, false]
 ]
 
 animalObjects = [];
@@ -33,9 +34,11 @@ for( var i=0; i<animalData.length; i+=1){
 	var newAnimal = new Animal(animalData[i][0], animalData[i][1], animalData[i][2], animalData[i][3], 
 		animalData[i][4], animalData[i][5], animalData[i][6], animalData[i][7], animalData[i][8], 
 		animalData[i][9], animalData[i][10], animalData[i][11], animalData[i][12], animalData[i][13],
-		animalData[i][14], animalData[i][15]);
+		animalData[i][14], animalData[i][15], animalData[i][16]);
 	animalObjects.push(newAnimal);
 }
+
+
 
 
 
