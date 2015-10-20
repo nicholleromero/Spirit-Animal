0f49de343ac4d11085a5d1cd428d08d3ab0a0d0b
John Cody Nemmer <jonemmer@hotmail.com>
2015-10-20 14:20:49 -0700

    Added local storage to Output.js. Added results from our index.html page and our questions.js page to output.js

--- output.js
+++ output.js
@@ -1,6 +1,19 @@
+
+var animalObjects = JSON.parse(localStorage.getItem('animals'));
+
+var winningAnimal = 0;
+
+
+for (var i = 0; i < (animalObjects.length); i++) {
+		if(animalObjects[i].score > winningAnimal) {
+		winningAnimal = animalObjects[i];
+	} 
+};
+
 var output = function() {
 	var hOne = document.getElementById('textMsg');
 	var msg = "Congradulations, " + localStorage.getItem('username') + '<br />' + "We've Found Your Spirit Animal" 
+						+ winningAnimal.animalName + '<img src= ' + winningAnimal.animalImage + ' />';
 	hOne.innerHTML = msg;
 }
 
