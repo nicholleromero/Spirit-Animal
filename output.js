60f02850890273c6188d165cd9774c0375f9d5d0
John Cody Nemmer <jonemmer@hotmail.com>
2015-10-21 11:34:26 -0700

    Added more functionalty

--- output.js
+++ output.js
@@ -1,8 +1,6 @@
-
 var animalObjects = JSON.parse(localStorage.getItem('animals'));
 
 var winningAnimal = 0;
-
 
 for (var i = 0; i < (animalObjects.length); i++) {
 		if(animalObjects[i].score > winningAnimal) {
@@ -12,9 +10,21 @@ for (var i = 0; i < (animalObjects.length); i++) {
 
 var output = function() {
 	var hOne = document.getElementById('textMsg');
-	var msg = "Congradulations, " + localStorage.getItem('username') + '<br />' + "We've Found Your Spirit Animal" 
-						+ winningAnimal.animalName + '<img src= ' + winningAnimal.animalImage + ' />';
-	hOne.innerHTML = msg;
+	var article = document.getElementById('animal-content');
+	var msgOne = "Congradulations, " + localStorage.getItem('username') + '<br />' + '<img src= ' + 
+						winningAnimal.animalImage + ' />' + '<br />' + "We've Found Your Spirit Animal " 
+						+ " The " + '<q>' + winningAnimal.animalName.toUpperCase() + '</q>';
+	hOne.innerHTML = msgOne;
+	var msgTwo = winningAnimal.animalInfo;
+	article.innerHTML = msgTwo;
 }
 
 output();
+
+var playAgain = function() {
+	 window.location.href = "index.html";
+}
+
+var again = document.getElementById('play-again');
+again.addEventListener('click', playAgain);
+
