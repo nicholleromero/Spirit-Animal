2981a78c6c94a79c6771acaafb7b5236b900b905
noahgribbin <noahgribbin@yahoo.com>
2015-10-23 13:25:15 -0700

    one of these days

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var initialClick = function(e) {
 	var userInput = function() {
 		username = document.getElementById('username').value;
 		var label = document.getElementById('labelTag');
-		var nameCorrect = "You Must Enter A Name";
+		var nameCorrect = "YOU MUST ENTER A NAME";
 		if(username.length === 0) {
 			label.innerHTML = nameCorrect;
 			userInput();
@@ -14,7 +14,7 @@ var initialClick = function(e) {
 		localStorage.setItem('username', username);
 		document.getElementById('username').style.visibility = "hidden";
 	}
-//Function Change the Name Label to a "String" Message preparing the user for the test. 
+//Function Change the Name Label to a "String" Message preparing the user for the test.
 	var loadQuestions = function() {
 	  var label = document.getElementById('labelTag');
 	  var msg = "Hello, " + username + "!" + '<br />' + " Are You Ready To Find Your Your Spirit Animal?";
@@ -27,10 +27,10 @@ var initialClick = function(e) {
 //Listener to listen for the enter or return button to be pressed.
 		window.addEventListener('keypress', function(e) {
 			if (e.keyCode == 13) startGame('keypress');
-		}); 
+		});
 		submitButton.addEventListener(('click' || keyCode == 13), startGame);
 }
-//Function to take our user to the next page/to the Spirit Animal Test 
+//Function to take our user to the next page/to the Spirit Animal Test
 var startGame = function (e) {
 	if (e.which === 1) e.preventDefault();
   window.location.href = "questions.html";
@@ -38,7 +38,7 @@ var startGame = function (e) {
 //Runs our event listener.
 var submitButton = document.getElementById('submitButton');
 submitButton.addEventListener(('click' || keyCode == 13), initialClick);
-	
+
 
 
 
