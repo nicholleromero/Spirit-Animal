d0c1cbe08a6ec824a3155af9eea7c2cb2f02e560
David Swaintek <dswaintek@gmail.com>
2015-10-23 13:01:22 -0700

    Cleaned code on beats.js

--- beats.js
+++ beats.js
@@ -17,29 +17,29 @@ var solfeggio;
 
       var context = new AudioContext(); // Create audio container
 
-      oscillator1 = context.createOscillator(); // Create bass guitar
-      oscillator1.type = "sine"; // Square wave
+      oscillator1 = context.createOscillator(); // Create osc 1
+      oscillator1.type = "sine"; // Sine wave
       oscillator2 = context.createOscillator(); // Create bass guitar
-      oscillator2.type = "sine"; // Square wave
-      gainNode = context.createGain(); // Create boost pedal
-      gainNode.connect(context.destination); // Connect boost pedal to amplifier
-      gainNode.gain.value = 0.7; // Set boost pedal to 30 percent volume
+      oscillator2.type = "sine"; // Sine wave
+      gainNode = context.createGain(); // Create gain to control volume
+      gainNode.connect(context.destination); // Connect gain to output
+      gainNode.gain.value = 0.7; // Set volume at 70% (oscillators are loud!)
       
-      pannerNode1 = context.createStereoPanner();
+      pannerNode1 = context.createStereoPanner(); // Pan oscillator 1 left
       pannerNode1.pan.value = -1;
 
-      pannerNode2 = context.createStereoPanner();
+      pannerNode2 = context.createStereoPanner(); // Pan oscillator 2 right
       pannerNode2.pan.value = 1;
 
-      oscillator1.connect(pannerNode1);
-      pannerNode1.connect(gainNode); // Connect bass guitar to boost pedal
-      oscillator2.connect(pannerNode2);
-      pannerNode2.connect(gainNode); // Connect bass guitar to boost pedal
+      oscillator1.connect(pannerNode1); //Connect oscillator1 to pan1
+      pannerNode1.connect(gainNode); // Connect pan 1 to gain
+      oscillator2.connect(pannerNode2); //Connect oscillator 2 to pan2
+      pannerNode2.connect(gainNode); // Connect pan 2 to gain
 
-var loadBeat = function (event) {
+var loadBeat = function (event) {  // Gets user input from radio buttons
   event.preventDefault();
 
-    if (document.getElementById('ut').checked == true) {
+    if (document.getElementById('ut').checked == true) { //Solfeggio selection
       solfeggio = ut;
     } else if (document.getElementById('re').checked == true) {
       solfeggio = re;
@@ -51,7 +51,7 @@ var loadBeat = function (event) {
       solfeggio = sol;
     }
 
-    if (document.getElementById('delta').checked == true) {
+    if (document.getElementById('delta').checked == true) { //Brainwave selection
       brainWave = delta;
     } else if (document.getElementById('theta').checked == true) {
       brainWave = theta;
@@ -65,26 +65,26 @@ var loadBeat = function (event) {
       brainWave = highGamma;
     }
 
-    var leftBeat = solfeggio - (brainWave / 2);
-    var rightBeat = solfeggio + (brainWave / 2);
+    var leftBeat = solfeggio - (brainWave / 2); // Determines left frequency
+    var rightBeat = solfeggio + (brainWave / 2); // Determines right frequency
 
-    oscillator1.frequency.value = leftBeat; // frequency in hertz
+    oscillator1.frequency.value = leftBeat; // Sets frequency in hertz
 
-    oscillator2.frequency.value = rightBeat; // frequency in hertz
+    oscillator2.frequency.value = rightBeat; // Sets frequency in hertz
       
 
-    oscillator1.start(); // Play bass guitar instantly
-    oscillator2.start();
+    oscillator1.start(); // Play oscillator 1
+    oscillator2.start(); // Play oscillator 2
   }
 
 var stopBeat = function() {
   event.preventDefault;
-    oscillator1.stop(); // Play bass guitar instantly
-    oscillator2.stop();
+    oscillator1.stop(); // Stop oscillator 1
+    oscillator2.stop(); // Stop oscillator 2
 }
 
-var launchBeat = document.getElementById('launchBeat');
+var launchBeat = document.getElementById('launchBeat'); //Launch button
 launchBeat.addEventListener('click', loadBeat);
 
-var stopBeats = document.getElementById('stopBeat');
+var stopBeats = document.getElementById('stopBeat'); //Stop button
 stopBeats.addEventListener('click', stopBeat);
